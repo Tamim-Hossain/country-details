718ad41727d856f70e0481878064fdce69f71427
tamim-hossain <tamim.cloud@gmail.com>
2021-03-25 00:06:29 +0600

    search option added on homepage

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,6 +7,7 @@ import {
 	CardContent,
 	CardMedia,
 	Container,
+	CssBaseline,
 	Grid,
 	TextField,
 	Typography,
@@ -14,6 +15,7 @@ import {
 import { makeStyles } from "@material-ui/core/styles";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
 
 const useStyles = makeStyles({
 	root: {
@@ -25,43 +27,71 @@ const useStyles = makeStyles({
 	content: {
 		height: 70,
 	},
+	search: {
+		marginBottom: "30px",
+	},
+	logo: {
+		height: "130px",
+		width: "150px",
+	},
 });
 
 const Main = () => {
-	const [data, setData] = useState([]);
 	const classes = useStyles();
+
+	const [countries, setCountries] = useState([]);
+	const [search, setSearch] = useState("");
+	const [filteredCountries, setFilteredCountries] = useState([]);
 
 	useEffect(() => {
 		fetch("https://restcountries.eu/rest/v2/all")
 			.then((res) => res.json())
-			.then((data) => setData(data));
+			.then((data) => setCountries(data));
 	}, []);
+
+	useEffect(() => {
+		const find = countries.filter((country) => country.name.toLowerCase().includes(search));
+		setFilteredCountries(find);
+	}, [countries, search]);
 
 	return (
 		<Container>
-			<TextField id="outlined-basic" label="Search Country" variant="outlined" placeholder="Enter country name.." />
-			<Grid container spacing={7}>
-				{data.map((country) => (
+			<CssBaseline />
+			<Grid className={classes.search} container justify="space-evenly" alignItems="center">
+				<img src={logo} alt="" className={classes.logo} />
+				<TextField
+					label="Search Country"
+					variant="outlined"
+					placeholder="Enter country name.."
+					defaultValue={search}
+					value={search}
+					onChange={(e) => setSearch(e.target.value)}
+				/>
+			</Grid>
+			<Grid container spacing={7} justify="space-around">
+				{filteredCountries.map((country) => (
 					<Grid item md={3} key={country.alpha3Code}>
 						<Card className={classes.root}>
 							<CardActionArea>
 								<CardMedia className={classes.media} image={country.flag} title="Contemplative Reptile" />
 								<CardContent className={classes.content}>
-									<Typography gutterBottom variant="h6" component="h1" align="center">
+									<Typography gutterBottom variant="h5" component="h1" align="center">
 										<Box fontWeight="fontWeightBold">{country.name}</Box>
 									</Typography>
 								</CardContent>
 							</CardActionArea>
 							<CardActions>
-								<Button
-									variant="contained"
-									color="primary"
-									disableElevation
-									component={Link}
-									to={`/${country.alpha2Code}`}
-								>
-									View Details
-								</Button>
+								<Grid container justify="center">
+									<Button
+										variant="contained"
+										color="primary"
+										disableElevation
+										component={Link}
+										to={`/${country.alpha2Code}`}
+									>
+										View Details
+									</Button>
+								</Grid>
 							</CardActions>
 						</Card>
 					</Grid>
